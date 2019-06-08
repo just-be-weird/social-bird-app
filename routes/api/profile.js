11404ed964c1e85447e8182a1d8556c86939fd4b
just-be-weird <abhi.chandresh@gmail.com>
2019-06-08 17:25:42 +0530

    Optimized code for updating and adding user profile details.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -55,24 +55,24 @@ route.post('/', [auth, [
     const profileFields = {};
     profileFields.user = req.user.id;
     // if (req.body.handle) profileFields.handle = req.body.handle;
-    if (req.body.company) profileFields.company = req.body.company;
-    if (req.body.website) profileFields.website = req.body.website;
-    if (req.body.location) profileFields.location = req.body.location;
-    if (req.body.bio) profileFields.bio = req.body.bio;
-    if (req.body.status) profileFields.status = req.body.status;
-    if (req.body.githubusername) profileFields.githubusername = req.body.githubusername;
+    if (req.body.company) profileFields.company = company;
+    if (req.body.website) profileFields.website = website;
+    if (req.body.location) profileFields.location = location;
+    if (req.body.bio) profileFields.bio = bio;
+    if (req.body.status) profileFields.status = status;
+    if (req.body.githubusername) profileFields.githubusername = githubusername;
     // Skills - Spilt into array
-    if (typeof req.body.skills !== 'undefined') {
-        profileFields.skills = req.body.skills.split(',');
+    if (typeof skills !== 'undefined') {
+        profileFields.skills = skills.split(',');        
     }
 
     // Social object (optional fields)
     profileFields.social = {};
-    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
-    if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
-    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
-    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
-    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+    if (req.body.youtube) profileFields.social.youtube = youtube;
+    if (req.body.twitter) profileFields.social.twitter = twitter;
+    if (req.body.facebook) profileFields.social.facebook = facebook;
+    if (req.body.linkedin) profileFields.social.linkedin = linkedin;
+    if (req.body.instagram) profileFields.social.instagram = instagram;
 
     try {
         let profile = await Profile.findOne({ user: req.user.id });
@@ -136,7 +136,7 @@ route.get('/user/:user_id', async (req, res) => {
 //@desc     Delete the user, profile & post for that user
 //@access   Private
 
-route.delete('/',auth , async (req, res) => {
+route.delete('/', auth, async (req, res) => {
     try {
         //Remove the profile
         await Profile.findOneAndRemove({ user: req.user.id });
