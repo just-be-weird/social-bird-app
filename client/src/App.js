b40fb843aa23f51ba7ad26e48792dbe832056a26
just-be-weird <abhi.chandresh@gmail.com>
2019-06-09 21:33:35 +0530

    Intial UI Setup.

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import Navbar from './components/layouts/Navbar';
+import Landing from './components/layouts/Landing';
+import './App.css';
+
+
+const App = () => (
+	<Fragment>
+		<Navbar />
+		<Landing />
+	</Fragment>
+);
+
+export default App;
