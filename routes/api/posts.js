4392facb40c119c26c91524dd0887aec3c33902d
just-be-weird <abhi.chandresh@gmail.com>
2019-06-09 19:15:07 +0530

    Code for liking a post.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -103,4 +103,34 @@ route.delete('/:id', auth, async (req, res) => {
     }
 });
 
+
+//@route    PUT api/posts/like/:id
+//@desc     Like a post
+//@access   Private
+
+route.put('/like/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post) {
+            return res.status(404).json({ msg: 'There is no post found.' });
+        }
+
+        //Check if the post been liked before
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+            return res.status(404).json({ msg: 'Post already been liked.' });
+        }
+
+        post.likes.unshift({ user: req.user.id });
+
+        await post.save();
+        res.json(post.likes);
+    } catch (error) {
+        console.error(error.message);
+        if (error.kind === 'ObjectId') {
+            return res.status(404).json({ msg: 'There is no post found.' });
+        }
+        res.status(500).send('Server Error!');
+    }
+});
+
 module.exports = route;
