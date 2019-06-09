3a05fc4a44b57721ca9f0090849b18ce2d6a5b50
just-be-weird <abhi.chandresh@gmail.com>
2019-06-09 19:45:00 +0530

    Code for commenting on a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -160,4 +160,39 @@ route.put('/unlike/:id', auth, async (req, res) => {
 });
 
 
+//@route    POST api/posts/comment/:id
+//@desc     Comment on a post
+//@access   Private
+
+route.post('/comment/:id', [
+    auth,
+    [
+        check('text', 'Text is required').not().isEmpty()
+    ]
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        const post = await Post.findById(req.params.id);
+
+        const newComment = {
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        };
+
+        post.comments.unshift(newComment);
+        await post.save();
+        res.json(post.comments);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error!');
+    }
+});
+
 module.exports = route;
