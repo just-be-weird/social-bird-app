d625c158c7a0a91cfb9b751cd27e185c2c37bec4
just-be-weird <abhi.chandresh@gmail.com>
2019-06-14 12:34:04 +0530

    Code for setting up redux thunk, middlewares, connect etc.

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,28 @@ import Navbar from './components/layouts/Navbar';
 import Landing from './components/layouts/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+//Redux 
+import { Provider } from 'react-redux';//This combines redux and react togather
+import store from './store';
+
 import './App.css';
 
 
 const App = () => (
-	<Router>
-		<Fragment>
-			<Navbar />
-			<Route exact path='/' component={ Landing } />
-			<section className="container">
-				<Switch>
-					<Route exact path='/login' component={Login}/>
-					<Route exact path='/register' component={Register}/>
-				</Switch>
-			</section>
-		</Fragment>
-	</Router>
+	<Provider store={store}>
+		<Router>
+			<Fragment>
+				<Navbar />
+				<Route exact path='/' component={Landing} />
+				<section className="container">
+					<Switch>
+						<Route exact path='/login' component={Login} />
+						<Route exact path='/register' component={Register} />
+					</Switch>
+				</section>
+			</Fragment>
+		</Router>
+	</Provider>
 );
 
 export default App;
